3295e2717c78a1d5c5a52964558663ddba7a6983
monkeyclass <30174727+monkeyclass@users.noreply.github.com>
2025-01-22 22:48:45 +0100

    authentication using bearer token

--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
+import os
+
 from sqlmodel import Session
 from typing import Annotated
-from fastapi import FastAPI, Depends
+from starlette import status
+from dotenv import load_dotenv
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 from database import create_db_and_tables, engine
 from models import EventCreate, Event
 
 app = FastAPI()
+security = HTTPBearer()
+load_dotenv()
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
-
 
 def get_session():
     with Session(engine) as session:
@@ -18,8 +24,23 @@ def get_session():
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
+auth_token = os.getenv("AUTH_TOKEN")
+
+async def validate_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    if credentials.credentials != auth_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return credentials.credentials
+
 @app.post("/event/")
-def create_hero(event: EventCreate, session: SessionDep) -> dict():
+def create_event(
+    event: EventCreate,
+    session: SessionDep,
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(validate_token)]
+) -> dict():
     db_event = Event(
         utc=event.utc,
         cog=event.cog,
