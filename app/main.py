1f7385769ebc4bef8f40dc74421164adad17ae4c
monkeyclass <30174727+monkeyclass@users.noreply.github.com>
2025-01-22 22:02:16 +0100

    session yield

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
 from sqlmodel import Session
+from typing import Annotated
+from fastapi import FastAPI, Depends
 
 from database import create_db_and_tables, engine
 from models import EventCreate, Event
@@ -10,23 +11,29 @@ app = FastAPI()
 def on_startup():
     create_db_and_tables()
 
-@app.post("/event/")
-def create_hero(event: EventCreate):
-    with Session(engine) as session:
-        db_event = Event(
-            utc=event.utc,
-            cog=event.cog,
-            nsat=event.nsat,
-            alt=event.alt,
-            ts=event.ts,
-            t=event.t,
-            sog=event.sog,
-            lat=event.loc.lat,
-            lon=event.loc.lon,
-        )
 
-        db_event = Event.model_validate(db_event)
-        session.add(db_event)
-        session.commit()
-        session.refresh(db_event)
-        return db_event
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+@app.post("/event/")
+def create_hero(event: EventCreate, session: SessionDep) -> dict():
+    db_event = Event(
+        utc=event.utc,
+        cog=event.cog,
+        nsat=event.nsat,
+        alt=event.alt,
+        ts=event.ts,
+        t=event.t,
+        sog=event.sog,
+        lat=event.loc.lat,
+        lon=event.loc.lon,
+    )
+
+    db_event = Event.model_validate(db_event)
+    session.add(db_event)
+    session.commit()
+    session.refresh(db_event)
+    return {}
